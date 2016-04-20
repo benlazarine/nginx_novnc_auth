0290885e6095702806df3272dfce337bfc666fe3
Julian Pistorius <julian@julianpistorius.com>
2016-04-20 12:07:22 -0700

    Added rudimentary checking of the HTTP_X_ORIGINAL_URI

--- auth_server.py
+++ auth_server.py
@@ -1,3 +1,5 @@
+from urlparse import urlparse, parse_qs
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -5,8 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def auth():
-    # return 'Go away', 401
-    return 'All good', 200
+    original_uri = request.environ.get('HTTP_X_ORIGINAL_URI', '')
+    uri_parts = urlparse(original_uri)
+    query_string = uri_parts.query
+    query_vars = parse_qs(query_string)
+    code_list = query_vars.get('code', '401')
+    if isinstance(code_list, list):
+        code = code_list[0]
+    else:
+        code = code_list
+    return 'Auth result', int(code)
 
 
 if __name__ == '__main__':
